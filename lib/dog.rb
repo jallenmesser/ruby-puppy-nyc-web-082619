6fdc82ff00e331402b1f5a8467aa3383c06d612e
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-28 12:51:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Add your code here
+
+class Dog 
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end 
+  
+  def self.print_all
+    print @@all
+  end
+  
+end
